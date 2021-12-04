0da780d1693f63e29126965aa6ad7ff349ca82b9
김창배 <cbkim93@naver.com>
2021-12-04 11:43:29 +0900

    프래그먼트의 동작 관련 코드 - onActivityCreated 함수 오버라이딩 - 화면을 전달, this 대신에 requireContext()를 사용

--- app/src/main/java/com/example/fragment_20211204/FirstFragment.kt
+++ app/src/main/java/com/example/fragment_20211204/FirstFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +15,13 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        
+        btnToast.setOnClickListener { 
+            Toast.makeText(requireContext(),"프래그먼트에서 토스트 띄우기",Toast.LENGTH_SHORT).show()
+        }
     }
 }
